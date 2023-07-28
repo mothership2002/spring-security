787c00f3f76c1a9863c6fb7ed5b476b6cd03bb60
mothership2002 <mothership2002@naver.com>
2023-07-28 22:00:53 +0900

    no message

--- src/main/java/hello/security/config/SecurityConfig.java
+++ src/main/java/hello/security/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package hello.security.config;
 
+import hello.security.config.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,11 +23,23 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/","/home").permitAll()
-			.antMatchers("/admin/**").hasRole("ADMIN")
-			.and()
-			.formLogin().disable();
+//		http.authorizeRequests()
+//			.antMatchers("/","/home").permitAll()
+//			.antMatchers("/admin/**").hasRole("ADMIN")
+//			.and()
+//			.formLogin().disable();
+
+		http.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.formLogin().disable()
+				.httpBasic().disable()
+				.apply();
+
 		return http.build();
+	}
+
+	public AuthenticationManager authenticationManager() {
+		return
 	}
 }
---
+++ src/main/java/hello/security/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,6 @@
+package hello.security.config.security;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+}
