ab8baf3b0f2e9dd6bfe95cae181691bf41190ea0
mothership2002 <mothership2002@naver.com>
2023-07-30 14:18:18 +0900

    no message

--- src/main/java/hello/security/config/JwtProperties.java
+++ src/main/java/hello/security/config/JwtProperties.java
@@ -1,12 +1,33 @@
 package hello.security.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
+@Setter
+@Getter
 @ConfigurationProperties("jwt")
 public class JwtProperties {
 
+    private String header;
     private String secretKey;
     private int expiredDate;
+
+    private long expiredSecond;
+
+    @PostConstruct
+    public void init() {
+        expiredSecond = toSecond(expiredDate);
+    }
+
+    private int toSecond(int expiredDate) {
+        return expiredDate * 24 * 60 * 60;
+    }
+
 }
--- src/main/java/hello/security/config/security/JwtProvider.java
+++ src/main/java/hello/security/config/security/JwtProvider.java
@@ -1,12 +1,42 @@
 package hello.security.config.security;
 
 import hello.security.config.JwtProperties;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
 
     private final JwtProperties properties;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = properties.getSecretKey().getBytes();
+        key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream().
+                map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expiredDate = now.plusSeconds(properties.getExpiredSecond());
+        return Jwts.builder()
+                .setSubject("auth")
+                .
+
+    }
 }
